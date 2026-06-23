79cf98aa832dd519ceccb0a25887fc30fcedefb4
srreeraj <psreeraj711@gmail.com>
2026-06-23 18:41:38 +0530

    created main.js file

---
+++ Kurtishop/static/js/main.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+if (menuBtn) {
+    menuBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+}
