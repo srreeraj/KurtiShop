8e8065397c0b481309d33ebd5771cc9f4e2769ed
srreeraj <psreeraj711@gmail.com>
2026-06-24 23:25:31 +0530

    created product list view and product detail view

--- Kurtishop/products/views.py
+++ Kurtishop/products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.object.filter(
+        is_active=True,
+        is_deleted=False
+    )
+
+    context = {
+        'product' : products
+    }
+
+    return render(
+        request,
+        'product/product_list.html',
+        context
+    )
+
+def product_detail(request,slug):
+    product = get_object_or_404(
+        Product,
+        slug=slug,
+        is_active=True,
+        is_deleted=False,
+    )
+
+    context = {
+        'product' : product
+    }
+
+    return render(
+        request,
+        'products/product_detail.html',
+        context
+    )
