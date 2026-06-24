a69a0a1ec5f155fbbfe8d77378414aaaf58b7ec3
srreeraj <psreeraj711@gmail.com>
2026-06-24 18:35:47 +0530

    created categories.models

--- Kurtishop/categories/models.py
+++ Kurtishop/categories/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    image = models.ImageField(
+        upload_to='categories/',
+        blank='True',
+        null='True',
+    )
+
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
