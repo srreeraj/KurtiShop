cfb3a6f18cf43b1f5f25711737e4b61e340e22ca
srreeraj <psreeraj711@gmail.com>
2026-06-24 22:34:16 +0530

    registered product, product color, product size models on product.admin

--- Kurtishop/products/admin.py
+++ Kurtishop/products/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
-
+from .models import (
+    Product,
+    ProductVariant,
+    ProductImage,
+    Size,
+    Color
+)
 # Register your models here.
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'is_active',
+        'is_featured'
+    )
+
+    list_filter = (
+        'category',
+        'is_active',
+        'is_featured'
+    )
+
+    search_fields = (
+        'name',
+        'sku'
+    )
+
+    perpopulated_fields = {
+        'slug' : ('name',)
+    }
+
+    inlines = [
+        ProductVariantInline,
+        ProductImageInline,
+    ]
+
+admin.site.register(Size)
+admin.site.register(Color)
