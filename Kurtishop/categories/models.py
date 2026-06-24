c80306f10977f5200efd58343e5ff13f04e91630
srreeraj <psreeraj711@gmail.com>
2026-06-24 18:51:43 +0530

    added is_deleted for soft deletion on category models

--- Kurtishop/categories/models.py
+++ Kurtishop/categories/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
 
     description = models.TextField(blank=True)
     is_active = models.BooleanField(blank=True)
+    is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
