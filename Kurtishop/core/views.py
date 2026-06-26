d523f1caccf31af50536dfb9d7f008d51b0b8503
srreeraj <psreeraj711@gmail.com>
2026-06-26 19:37:52 +0530

    added featured_products and new_arrivals on home backend

--- Kurtishop/core/views.py
+++ Kurtishop/core/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def home(request):
+    featured_products = Product.objects.filter(
+        is_featured = True,
+        is_active = True,
+        is_deleted = False,
+    )[:8]
+
+    new_arrivals = Product.objects.filter(
+        is_new_arrival = True,
+        is_active =True,
+        is_deleted = False,
+    )[:8]
+
+    context = {
+        'featured_products' : featured_products,
+        'new_arrivals' : new_arrivals
+    }
     return render(request, 'core/home.html')
