8722cf265752925e6415686346618bbcd44bae15
srreeraj <psreeraj711@gmail.com>
2026-06-30 13:32:07 +0530

    updated with products.admin by adding product tag and refining other models

--- Kurtishop/products/admin.py
+++ Kurtishop/products/admin.py
@@ -3,6 +3,7 @@ from .models import (
     Product,
     ProductVariant,
     ProductImage,
+    ProductTag,
     Size,
     Color,
     Material,
@@ -17,10 +18,12 @@ from .models import (
 class ProductVariantInline(admin.TabularInline):
     model = ProductVariant
     extra = 1
+    autocomplete_fields = ['size','color']
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
+    autocomplete_fields = ['color']
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -34,10 +37,16 @@ class ProductAdmin(admin.ModelAdmin):
         'is_active',
     )
 
+    filter_horizontal = (
+        'tags',
+    )
+
     list_filter = (
         'category',
         'material',
         'occasion',
+        'pattern',
+        'fit',
         'is_featured',
         'is_new_arrival',
         'is_active',
@@ -47,6 +56,9 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'sku',
         'description',
+        'material__name',
+        'occasion__name',
+        'category__name',
     )
 
     prepopulated_fields = {
@@ -58,34 +70,59 @@ class ProductAdmin(admin.ModelAdmin):
         ProductImageInline,
     ]
 
+    ordering = ('name',)
+
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Color)
 class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Occasion)
 class OccasionAdmin(admin.ModelAdmin):
+    list_dispaly = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Sleeve)
 class SleeveAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Neck)
 class NeckAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Pattern)
 class PatternAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 @admin.register(Fit)
 class FitAdmin(admin.ModelAdmin):
+    list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(ProductTag)
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    
