027420f02c548f72d596d15854b605241a85a3ef
srreeraj <psreeraj711@gmail.com>
2026-07-01 00:52:25 +0530

    category slug improved

--- Kurtishop/categories/models.py
+++ Kurtishop/categories/models.py
@@ -32,7 +32,15 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Category.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            
+            self.slug = slug
 
         super().save(*args, **kwargs)
     
