dd0624a67e88f329e79939c5e3e64e76ead81ecc
srreeraj <psreeraj711@gmail.com>
2026-06-30 14:11:48 +0530

    Improve save() in Product model to handle duplicate names with unique slugs

--- Kurtishop/products/models.py
+++ Kurtishop/products/models.py
@@ -193,7 +193,15 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Product.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            
+            self.slug = slug
         
         super().save(*args, **kwargs)
 
