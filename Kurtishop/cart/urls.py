11be89591d16445cbfefca55a363fbc61659b2b7
srreeraj <psreeraj711@gmail.com>
2026-07-09 00:56:04 +0530

    added drawer path to cart urls

--- Kurtishop/cart/urls.py
+++ Kurtishop/cart/urls.py
@@ -5,6 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
+    path('drawer/', views.cart_drawer, name='cart_drawer'),
     path('add/<int:variant_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update/<int:item_id>/', views.update_cart_quantity, name='update_cart_quantity'),
