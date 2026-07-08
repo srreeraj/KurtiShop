6df9fec64bd906030249bd1eb6af6fe5bfe357b0
srreeraj <psreeraj711@gmail.com>
2026-07-08 21:57:45 +0530

    created cart.js file

---
+++ Kurtishop/static/js/cart.js
@@ -0,0 +1,56 @@
+function getCSRFToken() {
+    return document.querySelector('[name=csrfmiddlewaretoken]').value;
+}
+
+function updateQuantity(itemId, change) {
+    const quantityInput = document.querySelector(`input[data-item-id]="${itemId}"]`);
+    let currentQty = parseInt(quantityInput.value);
+    let newQty = currentQty + change;
+
+    if (newQty < 1) {
+        if (confirm('Remove this item from cart?')) {
+            removeItem(itemId);
+        }
+        return;
+    }
+
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = `/cart/update/${itemId}`;
+    form.style.display = 'none';
+
+    // CSRF Token
+    const csrf = document.createElement('input');
+    csrf.type = 'hidden';
+    csrf.name = 'csrfmiddlewaretoken';
+    csrf.value = 'getCSRFToken()';
+    form.appendChild(csrf);
+
+    // Quantity
+    const qtyInput = document.createElement('input');
+    qtyInput.type = 'hidden';
+    qtyInput.name = 'quantity';
+    qtyInput.value = newQty;
+    form.appendChild(qtyInput);
+
+    document.body.appendChild(form);
+    form.submit()
+}
+
+function removeItem(itemId) {
+    if (confirm('Remove this item from cart?')) {
+        const form = document.createElement('form');
+        form.method = 'POST';
+        form.action = `/cart/remove/${itemId}/`;
+        form.style.display = 'none';
+
+        const csrf = document.createElement('input');
+        csrf.type = 'hidden';
+        csrf.name = 'csrfmiddlewaretoken';
+        csrf.value = getCSRFToken();
+        form.appendChild(csrf);
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+}
