087981516088586e0b0346e089fb566f9164ecd8
srreeraj <psreeraj711@gmail.com>
2026-07-08 16:11:39 +0530

    syntax error fixed

--- Kurtishop/config/urls.py
+++ Kurtishop/config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
 ]
 
