2c3314f066a63ff3e79b89400ded7beecd99bd88
srreeraj <psreeraj711@gmail.com>
2026-07-08 13:12:11 +0530

    Add helper function for cart creation and item count in navbar

---
+++ Kurtishop/cart/utils.py
@@ -0,0 +1,16 @@
+from django.Http import HttpRequest
+from .models import Cart
+
+def get_or_create_cart(request: HttpRequest):
+    """Get or create a cart for the current session."""
+    if not request.session.session_key:
+        request.session.create()
+
+    session_key = request.session.session_key
+    cart, created = Cart.objects.get_or_create(session_key=session_key)
+    return cart
+
+def get_cart_item_count(request: HttpRequest):
+    """For showing the count in navbar"""
+    cart = get_or_create_cart(request)
+    return cart.items.count()
