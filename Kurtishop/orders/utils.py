c7e94a2b098a907f5197c412dbcb794d4cb646d4
srreeraj <psreeraj711@gmail.com>
2026-07-10 23:57:18 +0530

    created orders.utils

---
+++ Kurtishop/orders/utils.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+import razorpay
+
+def get_razorpay_client():
+    return razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
+def create_razorpay_order(amount_in_paise):
+    client = get_razorpay_client()
+    data = {
+        "amount": amount_in_paise,
+        "currency": "INR",
+        "payment_capture": "1"
+    }
+    return client.order.create(data)
