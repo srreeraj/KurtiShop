ac72e050322b54ce6a6a8695f920a854533fa70c
srreeraj <psreeraj711@gmail.com>
2026-07-11 17:13:41 +0530

    removed payment related urls from the orders.urls

--- Kurtishop/orders/urls.py
+++ Kurtishop/orders/urls.py
@@ -6,8 +6,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
-    path('payment-success/', views.payment_success, name='payment_success'),
-    path('payment-failed/', views.payment_failed, name='payment_failed'),
     path('order-success/<str:order_number>/', views.order_success, name='order_success'),
     path('orders/<str:order_number>/', views.order_detail, name='order_detail'),
 ]
