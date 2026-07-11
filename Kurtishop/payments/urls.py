d1aecdf805ae1010fea6a3529ca50f6863bda4eb
srreeraj <psreeraj711@gmail.com>
2026-07-11 20:07:23 +0530

    created payments.urls

---
+++ Kurtishop/payments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "payments"
+
+urlpatterns = [
+    path('initiate/<str:order_number>', views.initiate_payment),
+    path('webhook/',views.razorpay_webhook, name="razorpay_webhook"),
+]
