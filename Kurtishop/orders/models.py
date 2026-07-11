23dc8e5520a797dfc3751cf772ccac8888d17f60
srreeraj <psreeraj711@gmail.com>
2026-07-11 16:55:29 +0530

    updated the order item ordering in descending

--- Kurtishop/orders/models.py
+++ Kurtishop/orders/models.py
@@ -163,7 +163,7 @@ class OrderItem(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_at"]
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f"{self.order.order_number} - {self.variant}"
