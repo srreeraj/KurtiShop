4f4dabf8e00ddd2a79765a505374ddb6ef95c3a0
srreeraj <psreeraj711@gmail.com>
2026-07-11 12:49:44 +0530

    create order.urls path on config.urls

--- Kurtishop/config/urls.py
+++ Kurtishop/config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls'))
 ]
 
 if settings.DEBUG:
