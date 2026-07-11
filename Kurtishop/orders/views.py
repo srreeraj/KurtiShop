6013912e3c9b8a2f963f8e1724f7651495271850
srreeraj <psreeraj711@gmail.com>
2026-07-11 16:47:39 +0530

    removed payment related function and variable from orders.views

--- Kurtishop/orders/views.py
+++ Kurtishop/orders/views.py
@@ -11,6 +11,10 @@ from .utils import create_razorpay_order
 
 def checkout(request):
     session_key = request.session.session_key
+    if not session_key:
+        request.session.create()
+        session_key = request.session.session_key
+
     cart = get_object_or_404(Cart, session_key=session_key)
 
     if not cart.items.exists():
@@ -27,43 +31,16 @@ def checkout(request):
                 **order_data,
                 'subtotal' : subtotal,
                 'grand_total' : subtotal,
+                'payment_method' : 'online',
             })
-            # Create Razorpay Order
-            razorpay_order = create_razorpay_order(
-                amount_in_paisa=int(order.grand_total * 100)
-            )
-            order.razorpay_order_id = razorpay_order['id']
-            order.save()
 
-            context = {
-                'order' : order,
-                'razorpay_key' : 'settings.RAZORPAY_KEY_ID',
-                'razorpay_order_id' : razorpay_order['id'],
-                'amount' : int(order.grand_total * 100) 
-            }
-
-            return render(request, 'orders/payment_page.html', context)
+            return render(request, 'orders/payment_page.html',order_number=order.order_number)
 
     else:
         form = OrderForm()
 
     context = {'form' : form , 'cart' : cart}
     return render(request, 'orders/checkout.html', context)
-
-def payment_success(request):
-    razorpay_payment_id = request.GET.get('razorpay_payment_id')
-    razorpay_order_id = request.GET.get('razorpay_order_id')
-
-    order = get_object_or_404(Order, razorpay_order_id=razorpay_order_id)
-    order.payment_status = Order.PaymentStatus.PAID
-    order.razorpay_payment_id = razorpay_payment_id
-    order.order_status = Order.OrderStatus.CONFIRMED
-    order.save()
-
-    return redirect('orders:order_success', order_number=order.order_number)
-
-def payment_failed(request):
-    return render(request, 'orders/payment_failed.html')
 
 
 def order_success(request, order_number):
