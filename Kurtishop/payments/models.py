8b8f5c7067f51c9ff87453328573f0e91a133ac3
srreeraj <psreeraj711@gmail.com>
2026-07-11 17:24:54 +0530

    create Payment model

--- Kurtishop/payments/models.py
+++ Kurtishop/payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from orders.models import Order
 # Create your models here.
+
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+
+    razorpay_order_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_signature = models.CharField(max_length=200, blank=True, null=True)
+
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(
+        max_length=20,
+        choices=[('pending', 'Pending'), ('success', 'Success'), ('failed', 'Failed')],
+        default='pending'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Payment - {self.order.order_number}"
