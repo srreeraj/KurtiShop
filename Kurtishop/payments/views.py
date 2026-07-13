2fd48b7528daef8a3bfcc92290cf9970e921d4bc
srreeraj <psreeraj711@gmail.com>
2026-07-14 01:30:58 +0530

    removed the initiate_payment view

--- Kurtishop/payments/views.py
+++ Kurtishop/payments/views.py
@@ -9,36 +9,6 @@ from .models import Payment
 from .utils import create_razorpay_order, get_razorpay_client
 
 
-def initiate_payment(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
-
-    razorpay_order = create_razorpay_order(int(order.grand_total * 100))
-
-    payment, created = Payment.objects.get_or_create(
-        order= order,
-        defaults={
-            "razorpay_order_id": razorpay_order["id"],
-            "amount": order.grand_total,
-        }
-    )
-
-    if not created:
-        payment.razorpay_order_id = razorpay_order["id"]
-        payment.amount = order.grand_total
-        payment.status = "pending"
-        payment.save()
-
-    order.razorpay_order_id = razorpay_order['id']
-    order.save()
-
-    context = {
-        'order': order,
-        'razorpay_key': settings.RAZORPAY_KEY_ID,
-        'razorpay_order_id': razorpay_order['id'],
-        'amount': int(order.grand_total * 100),
-    }
-    return render(request, 'payments/payment_page.html', context)
-
 
 @csrf_exempt
 def razorpay_webhook(request):
@@ -70,7 +40,7 @@ def razorpay_webhook(request):
                     payment.razorpay_signature = signature
                     payment.status = 'success'
                     payment.success
-                    
+
             return JsonResponse({'status': 'success'})
         except Exception:
             return JsonResponse({'status': 'error'}, status=400)
