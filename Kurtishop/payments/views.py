7267e7370a2e302b09fad19dbb9de0ca433b0e62
srreeraj <psreeraj711@gmail.com>
2026-07-14 01:44:32 +0530

    created verify payment view on the payment views

--- Kurtishop/payments/views.py
+++ Kurtishop/payments/views.py
@@ -3,12 +3,47 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 import json
 from orders.models import Order
 from .models import Payment
 from .utils import create_razorpay_order, get_razorpay_client
 
 
+@require_POST
+def verify_payment(request):
+    try:
+        data = json.loads(request.body)
+        client = get_razorpay_client()
+        params = {
+            'razorpay_order_id': data.get('razorpay_order_id'),
+            'razorpay_payment_id' : data.get('razorpay_payment_id'),
+            'razorpay_signature' : data.get('razorpay_signature'),
+        }
+        client.utility.verify_payment_signature(params)
+
+        order = get_object_or_404(Order, order_number=data.get('order_number'))
+        order.payment_status = Order.PaymentStatus.PAID
+        order.razorpay_payment_id = params['razorpay_payment_id']
+        order.order_status = Order.OrderStatus.CONFIRMED
+        order.save()
+
+        payment = Payment.objects.filter(order=order).first()
+
+        if payment:
+            payment.razorpay_payment_id = params['razorpay_payment_id']
+            payment.razorpay_signature = params['razorpay_signature']
+            payment.status = 'success'
+            payment.save()
+        
+        return JsonResponse({
+            'status': 'success',
+            'redirect_url': reverse('orders:order_success', args=[order.order_number])
+        })
+    except razorpay.errors.SignatureVerificationError:
+        return JsonResponse({'status': 'error', 'message': 'Signature verification failed'}, status=400)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
 
 @csrf_exempt
 def razorpay_webhook(request):
