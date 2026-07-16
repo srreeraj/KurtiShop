03a3865c66f4ac5eefbd0bd429ce71707b4e8c43
srreeraj <psreeraj711@gmail.com>
2026-07-16 15:02:36 +0530

    update home views for listing categories perfectly

--- Kurtishop/core/views.py
+++ Kurtishop/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
 from categories.models import Category
+from django.db.models import Count, Q
 
 # Create your views here.
 
@@ -9,19 +10,21 @@ def home(request):
     categories = Category.objects.filter(
         is_active = True,
         is_deleted = False,
-    )
+    ).annotate(
+        product_count=Count('products', filter=Q(products__is_active=True, product__is_deleted=False))
+    ).order_by('name')[:12]
 
     featured_products = Product.objects.filter(
         is_featured = True,
         is_active = True,
         is_deleted = False,
-    )[:8]
+    ).select_related('category')[:8]
 
     new_arrivals = Product.objects.filter(
         is_new_arrival = True,
         is_active =True,
         is_deleted = False,
-    )[:8]
+    ).select_related('category')[:8]
 
     context = {
         'categories' : categories,
