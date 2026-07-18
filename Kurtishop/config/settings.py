9ca8887895f39c8be3dcb0b23b5c11bbe8ef896c
srreeraj <psreeraj711@gmail.com>
2026-07-18 22:23:50 +0530

    name error fixed DB_HOST -> DB_PORT on PORT

--- Kurtishop/config/settings.py
+++ Kurtishop/config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
