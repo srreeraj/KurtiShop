556faeb0bbe0e796b253f68e40b59c482a7d3487
srreeraj <psreeraj711@gmail.com>
2026-07-22 00:50:43 +0530

    created categories.views for the crud operation on the admin side

--- Kurtishop/categories/views.py
+++ Kurtishop/categories/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+from .models import Category
+from .forms import CategoryForm
 # Create your views here.
+
+@login_required
+@user_passes_test(lambda u : u.is_staff)
+def category_list(request):
+    categories = Category.objects.filter(is_deleted=True).select_related('parent')
+    context = {
+        'categories' : categories,
+        'page_title' : 'Categories'
+    }
+    return render(request, 'dashboard/categories/list.html', context)
+
+
+@login_required
+@user_passes_test(lambda u : u.is_staff)
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Category created successfully')
+            return redirect('dashboard:category_list')
+    else:
+        form = CategoryForm()
+
+    context = {
+        'form' : form,
+        'page_title' : 'Create Category'
+    }
+    return render(request, 'dashboard/categories/form.html', context)
+
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk, is_deleted=False)
+    
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, request.FILES, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Category updated successfully!')
+            return redirect('dashboard:category_list')
+    else:
+        form = CategoryForm(instance=category)
+
+    context = {
+        'form': form,
+        'category': category,
+        'page_title': 'Edit Category'
+    }
+    return render(request, 'dashboard/categories/form.html', context)
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk, is_deleted=False)
+    category.is_deleted = True
+    category.save()
+    messages.success(request, 'Category deleted successfully!')
+    return redirect('dashboard:category_list')
+
