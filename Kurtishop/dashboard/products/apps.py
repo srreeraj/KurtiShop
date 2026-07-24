49239b99f65a4d92cce4be528922cfbac0106b9a
srreeraj <psreeraj711@gmail.com>
2026-07-24 15:26:28 +0530

    added label on app in the dashboard.product for avoid conflict on app name

--- Kurtishop/dashboard/products/apps.py
+++ Kurtishop/dashboard/products/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class DashboardProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashboard_products'
+    name = 'dashboard.products'
+    label = 'dashboard_products'
