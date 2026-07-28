8c2559f9e334bcee5f5058cc9b3778fa68bf0812
srreeraj <psreeraj711@gmail.com>
2026-07-29 00:22:15 +0530

    added cloudinary credentails on settings for the connect with cloudinary cloud storage

--- Kurtishop/config/settings.py
+++ Kurtishop/config/settings.py
@@ -168,3 +168,9 @@ ADMIN_EMAIL = env('EMAIL_HOST_USER')
 
 # Recommended
 SITE_URL = 'https://yourdomain.com'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME' : env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY' : env('CLOUDINARY_API_KEY'),
+    'API_SECRET' : env('CLOUDINARY_API_SECRET'),
+}
