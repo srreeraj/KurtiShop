502f30eed163f39368b8e78fd92b85e80d8755a9
srreeraj <psreeraj711@gmail.com>
2026-07-29 00:23:22 +0530

    added default file storage connected with cloudinary on settings

--- Kurtishop/config/settings.py
+++ Kurtishop/config/settings.py
@@ -174,3 +174,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY' : env('CLOUDINARY_API_KEY'),
     'API_SECRET' : env('CLOUDINARY_API_SECRET'),
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
