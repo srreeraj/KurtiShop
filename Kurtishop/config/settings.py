6d5d03c7d81f5f601e98970637a6d9e50e8bf891
srreeraj <psreeraj711@gmail.com>
2026-07-29 15:25:52 +0530

    added trycloudflare host

--- Kurtishop/config/settings.py
+++ Kurtishop/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '.trycloudflare.com'])
 
 
 # Application definition
