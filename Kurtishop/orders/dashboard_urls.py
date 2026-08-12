ec08de7f836f0d2e37fce2d6210d7188517e9ade
srreeraj <psreeraj711@gmail.com>
2026-08-13 01:22:53 +0530

    created view for fullfill exchange

--- Kurtishop/orders/dashboard_urls.py
+++ Kurtishop/orders/dashboard_urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("", views.order_list, name="order_list"),
     path("<str:order_number>/", views.order_detail, name="order_detail"),
     path("<str:order_number>/invoice/", views.download_invoice, name="download_invoice"),
+    path(
+        "<str:order_number>/return/<int:return_id>/fulfill-exchange/",
+        views.fulfill_exchange,
+        name="fulfill_exchange",
+    ),
 ]
