2faabc4fe94acd0b8d886e29946b4e9eda89e831
srreeraj <psreeraj711@gmail.com>
2026-08-13 15:40:02 +0530

    added urls for pincodes on products.dashboard_urls

--- Kurtishop/products/dashboard_urls.py
+++ Kurtishop/products/dashboard_urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("create/", views.product_create, name="product_create"),
     path("<int:pk>/edit/", views.product_edit, name="product_edit"),
     path("<int:pk>/delete/", views.product_delete, name="product_delete"),
+    path("pincodes/", views.pincode_list, name="pincode_list"),
+    path("pincodes/create/", views.pincode_create, name="pincode_create"),
+    path("pincodes/<int:pk>/edit/", views.pincode_edit, name="pincode_edit"),
+    path("pincodes/<int:pk>/delete/", views.pincode_delete, name="pincode_delete"),
 ]
